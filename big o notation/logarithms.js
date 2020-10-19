45551feb36144f6ad5c33224525105e081d68ff3
lee-injae <ilee.injae@gmail.com>
2020-10-19 17:42:17 -0400

    logarithm examples added

---
+++ big o notation/logarithms.js
@@ -0,0 +1,9 @@
+//the logarithm of a number roughly measures the number of times 
+//you can divide that number by 2 before you get a value that is 
+//less than or equal to 1.
+
+//ex) log 8 = 3 because 2^3 = 8 
+
+// * all these apply only with the log subscript with 2. 
+
+ 
