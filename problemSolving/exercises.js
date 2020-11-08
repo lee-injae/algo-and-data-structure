615c3c43138c0ee4ec9da5393ccae0bc22f3fbfd
lee-injae <ilee.injae@gmail.com>
2020-11-08 06:16:53 -0500

    exercises-sameFrequency updated

--- problemSolving/exercises.js
+++ problemSolving/exercises.js
@@ -12,4 +12,58 @@
 
 function sameFrequency(num1, num2) {
 
+    let s1 = num1.toString()
+    let s2 = num2.toString()
+    if (s1.length !== s2.length) return false;
+
+    let counter = {}
+    // for (let digit of s1) {
+    //     counter[digit] = (counter[digit] || 0) + 1 
+    // }
+    //or 
+    for(i = 0; i < s1.length; i++){
+        let digit = s1[i]
+        counter[digit] ? counter[digit] += 1 : counter[digit] = 1;   
+    }
+
+    for (i = 0; i < s2.length; i++){ 
+        let digit = s2[i]
+        if (!counter[digit]) {
+            return false
+        }  else {
+            counter[digit] -= 1
+        }
+    }
+   
+    console.log(counter)
+    return true
 }
+
+//another way
+function sameFrequency(num1, num2) {
+    let s1 = num1.toString()
+    let s2 = num2.toString()
+    if (s1.length !== s2.length) return false;
+
+    let counter1 = {}
+    let counter2 = {}
+    for (let digit of s1) {
+        counter1[digit] = (counter1[digit] || 0) + 1   
+    }
+    for (let digit of s2) {
+        counter2[digit] = ++counter2[digit] || 1
+    }
+
+    for (let digit in counter1) {
+        if (!(digit in counter2)) {
+            return false
+        } 
+        if (counter1[digit] !== counter2[digit]) {
+            return false
+        } 
+    }
+    return true
+}
+
+
+sameFrequency(1233,3213)
