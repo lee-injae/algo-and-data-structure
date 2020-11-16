40d9cff4f8ece1b104df402894cea511a8778da8
lee-injae <ilee.injae@gmail.com>
2020-11-16 18:07:36 -0500

    isSubsequence

--- problemSolving/exercises.js
+++ problemSolving/exercises.js
@@ -216,7 +216,20 @@ function averagePair(arr, avg) {
         }
     return false
 }
-        
+
+//solution from Colt
+function averagePair(arr, num){
+    let start = 0 
+    let end = arr.length -1;
+    while (start < end) {
+        let avg = (arr[start]+arr[end]) / 2
+        if (avg === num) return true;
+        else if (avg < num) start++
+        else end--
+    }
+    return false
+}
+
 averagePair([1,3,3,5,6,7,10,12,19], 8)
         
 
@@ -229,6 +242,50 @@ averagePair([1,3,3,5,6,7,10,12,19], 8)
 //isSubsequence('hello', 'hello world') //true
 //isSubsequence('sing, 'sting') // true
 //isSubsequence('abc', 'abracadabra') // true
-//isSubsequence('abc', 'acb') // flase(order matters)
+//isSubsequence('abc', 'acb') // false(order matters)
 
+//Time Complexity: O(N+M)
+//Space Complexity: O(1)
+
+//my first trial -> works
+function isSubsequence(str1, str2) {
+    if (str1.length > str2.length) return false;
+
+    let i = 0 
+    let j = 0 
+    while (i < str1.length ) {
+        if (str1[i] === str2[j]) {
+            i++;
+            j++;
+        } else if (j === str2.length)  {
+            return false
+        } else {
+            j++
+        }
+    }
+    return true
+}
+
+//Colt's answer - Iterative
+function isSubsequence(str1, str2){
+    var i = 0;
+    var j = 0;
+    if (!str1) return true;
+    while ( j < str2.length) {
+        if (str2[j] === str1[i]) i++;
+        if (i === str1.length) return true;
+        j++
+    }
+    return false;
+}
+
+// Recursive but not O(1) Space
+function isSubsequence(str1, str2) {
+    if(str1.length) return true
+    if(str2.length) return false
+    if(str2[0] === str1[0]) return isSubsequence(str1.slice(1));
+    return isSubsequence(str1, str2.slice(1))
+}
+
+isSubsequence('abc', 'abracadabra')
 
